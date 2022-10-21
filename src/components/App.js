acf011193faa95df18d61fe98478bedc408033f9
Andrew Cook <andrewjcook93@gmail.com>
2022-10-21 11:08:41 -0500

    first bit of styling - header, background, footer

--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,7 @@ import {Outlet} from 'react-router-dom';
 const App = () => {
   return (
     <div>
-      <header>Movie Mayhem</header>
+      <header><h1><a href="https://moviemayhem.netlify.app">Movie Mayhem</a></h1></header>
       <Navbar />
       <Outlet />
       <footer>
